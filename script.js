63a269530e6f971001fb619422512f3a9926b443
Aneesh Awadhiya <aneesh.awadhiya@gmail.com>
2023-09-20 00:27:10 +0530

    fixing the responsive issue

--- script.js
+++ script.js
@@ -188,6 +188,10 @@ var tl4 = gsap.timeline({
 
 window.onload = function () {
     document.getElementById("preloader").style.display = "none";
+
+    
     
     document.getElementById("main-content").style.display = "block";
+
+    
 };
